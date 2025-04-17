e0dd04fa64c43f4903bd4b9f82da9f1c84bd4cc3
thisme8 <stutiupreti8@gmail.com>
2025-04-17 21:24:43 +0545

    new

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
   server: {
+    historyApiFallback: true,
     proxy: {
       "/api": "http://localhost:5000", // Proxy API requests to your backend
     },
